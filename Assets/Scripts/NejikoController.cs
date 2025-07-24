346ae6d630dc44a65e920990e1cc22ec869a8c19
saku-kami <ttc2550SG0022@edu.tech.ac.jp>
2025-07-24 11:38:47 +0900

    Nejiko ステージ移動
    ねじこがステージ上で左右にいどうする
    ステージが長くなる

---
+++ Assets/Scripts/NejikoController.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NejikoController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //1.プレイヤーのキー入力を受け取る
+    //2.キー入力の方向に移動する
+    //3.移動方向に合わせてアニメーションを再生する
+
+    CharacterController controller;
+    Vector3 moveDirection = Vector3.zero;
+    public float speed = 0f;
+    Animator animator;
+    //ジャンプの高さを決める変数
+    public float jumpPower = 0f;
+
+    //重力の強さを決める変数
+    public float gravityPower = 0;
+
+    //ラインの数の最大値
+    int MaxLine = 2;
+    //ラインの数の最小値
+    int MinLine = -2;
+    //ライン間の距離
+    float LineWidth = 1.0f;
+    //移動先のライン
+    int targetLine = 0;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            //ねじ子がジャンプをおこなう処理
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveDirection.y = jumpPower;
+            }
+        }
+
+        //1フレーム毎に前進する距離の更新
+        float movePowerZ = moveDirection.z + (speed * Time.deltaTime);
+        //更新した距離と現在地の差分距離の計算
+        moveDirection.z = Mathf.Clamp(movePowerZ, 0f , speed);
+
+        //x方向は目標のポジションまでの差分で速度を出す
+        float ratioX = (targetLine * LineWidth - transform.position.x) / LineWidth;
+        moveDirection.x = ratioX * speed;
+
+        //右レーンの切り替え
+        if(Input.GetKeyDown("right") || Input.GetKeyDown("d"))
+        {
+            if(controller.isGrounded && targetLine < MaxLine)
+            {
+                targetLine = targetLine + 1;
+            }
+        }
+        //左レーンの切り替え
+        if (Input.GetKeyDown("left") || Input.GetKeyDown("a"))
+        {
+            if(controller.isGrounded && targetLine > MinLine)
+            {
+                targetLine = targetLine - 1;
+            }
+        }
+
+
+        /*
+        if (Input.GetAxis("Vertical") > 0.0f)
+        {
+            //ねじ子が前進する処理
+            moveDirection.z = Input.GetAxis("Vertical") * speed;
+        }
+        else
+        {
+            moveDirection.z = 0.0f;
+        }
+        */
+
+        //Horizontal(左右入力)があれば、ねじこを回転させる
+        //transform.Rotate(0, Input.GetAxis("Horizontal") * 3f, 0);
+
+
+
+        //キャラクターが重力で落下する処理
+        moveDirection.y = moveDirection.y - gravityPower * Time.deltaTime;
+
+        //移動量をTransformに変換する
+        Vector3 globalDirection = transform.TransformDirection(moveDirection);
+
+        //Controllerに移動量を渡す
+        controller.Move(globalDirection * Time.deltaTime);
+
+        //ねじこのアニメーションを最新する
+        animator.SetBool("run", moveDirection.z > 0f);
+    }
+}
